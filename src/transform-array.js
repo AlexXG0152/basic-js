e36527021a6048c1022a7d99c542f2d1725eed71
AlexXG0152 <34213814+AlexXG0152@users.noreply.github.com>
2021-09-19 16:15:45 +0300

    feat: add solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,40 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+  if (!Array.isArray(arr)) { // check arr type
+      throw Error("\'arr\' parameter must be an instance of the Array!");
+    }
+  let newArray = [...arr]; // create new Array for transforming
+  
+  // first case ("--discard-next")
+  if (newArray.includes("--discard-next")) {
+      newArray.splice((newArray.indexOf("--discard-next")) + 1, 1); // discard-next value
+      newArray.splice(newArray.indexOf("--discard-next"), 1); // delete control sequence
+      if (newArray.includes("--discard-prev")) newArray.splice(newArray.indexOf("--discard-prev"), 1); // delete control sequence
+      if (newArray.includes("--double-next")) newArray.splice(newArray.indexOf("--double-next"), 1); // delete control sequence
+      if (newArray.includes("--double-prev")) newArray.splice(newArray.indexOf("--double-prev"), 1); // delete control sequence
+  }
+  
+  // second case ("--double-next")
+  if (newArray.includes("--double-next")) {
+      if (newArray.indexOf("--double-next")+1 == newArray.length) newArray.splice((newArray.indexOf("--double-next")), 1); // double-next value
+      else newArray.splice(newArray.indexOf("--double-next"), 1, newArray[newArray.indexOf("--double-next")+1]); //delete control sequence
+  }
+  
+  // third case ("--double-prev")
+  if (newArray.includes("--double-prev")) {
+      if (newArray.indexOf("--double-prev") == 0) newArray.splice((newArray.indexOf("--double-prev")), 1); // check if not first
+      else newArray.splice(newArray.indexOf("--double-prev"), 1, newArray[newArray.indexOf("--double-prev")-1]); // replace control sequence by value
+  }
+  
+  // fourth case ("--discard-prev")
+  if (newArray.includes("--discard-prev")) {
+      if (newArray.indexOf("--discard-prev") == 0) newArray.splice((newArray.indexOf("--discard-prev")), 1); // --discard-prev value 
+      else {
+          newArray.splice((newArray.indexOf("--discard-prev")) - 1, 1); // discard-prev value
+          newArray.splice(newArray.indexOf("--discard-prev"), 1); //delete control sequence
+      }
+  }
+  return newArray
 }
